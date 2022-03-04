ed42d522dcab31695f9f36f1ef6e0791a62bdf8b
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-03-04 14:03:25 +0800

    Remove some steps since the commit was already reverted

--- revert_commits_to.ps1
+++ revert_commits_to.ps1
@@ -1,11 +1,6 @@
 #!/usr/bin/env pwsh
 param($mode)
 
-if ($mode -eq "strict" || $mode -eq "gone") {
-	& git revert 0daa8ded
-	Write-Output "Reverted to MIT license."
-}
-
 if ($mode -eq "gone") {
 	& git revert 86a573af
 	Write-Output "Removed the license."
